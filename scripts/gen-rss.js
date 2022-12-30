c7baa72905e8dc152bd8f718bd8d4ff18e4987ac
elizabethzhu1 <121363128+elizabethzhu1@users.noreply.github.com>
2022-12-30 00:45:57 -0500

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Elizabeth Zhu',
+    site_url: 'https://elizabeth-zhu.com',
+    feed_url: 'https://elizabeth-zhu/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
